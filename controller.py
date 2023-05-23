5199636019e7f804719bedfe6aff9c863743a94d
Your Name <you@example.com>
2023-05-24 00:36:10 +0400

    add 1, 2, 4 commands

---
+++ controller.py
@@ -0,0 +1,23 @@
+import view, model
+def start():
+    while True:
+        view.show_help()
+        command = input('Введите команду:')
+        if command == '1':
+            data = model.get_all_data()
+            view.show_contacts(data)
+        elif command == '2':
+            name = input('Введите имя контакта:')
+            syrname = input('Введите фамилию контакта:')
+            number_phone = input('Введите номер телефона:')
+            contact =  f"{name} {syrname} {number_phone}"
+            status = model.add_contact(contact)
+            view.show_status(status)
+        elif command == '4':
+            break
+            
+
+
+
+
+    
