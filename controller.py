2ea19cce91ec6d4129ba5f3dfdcce9f2a9fdc84e
Anton_Kondratev <elkondro@gmail.com>
2023-05-25 03:02:14 +0700

    bugfix

--- controller.py
+++ controller.py
@@ -8,7 +8,7 @@ def start():
         if command == '1':
             data = model.get_all_data()
             view.show_contacts(data)
-        elif command == '2'    
+        elif command == '2':
             name = input('Введите имя контакта: ').replace(" ", "")
             surname = input('Введите фамилию контакта: ').replace(" ", "")
             otche = input('Введите отчество конткта: ').replace(" ", "")
@@ -19,7 +19,7 @@ def start():
         elif command == '3':
             find = input('Введите что искать:')
             result = model.search(find)
-            view.show_contacts(result) 
+            view.show_contacts(result)
         elif command == '4':
             id = input('Введите id контакта')
             view.show_contacts(model.get_contact_by_id(id))
@@ -27,11 +27,11 @@ def start():
             what = input('Введите цифру')
             status = model.change_contact(id, what)
             view.show_status(status)
-        elif command == '5': 
+        elif command == '5':
             id = input('Введите id контакта')
             status = model.del_contact(id)
-            view.show_status(status)          
-        elif command == '6': 
-            view.show_menu()   
-        elif command == '7': 
+            view.show_status(status)
+        elif command == '6':
+            view.show_menu()
+        elif command == '7':
             break
